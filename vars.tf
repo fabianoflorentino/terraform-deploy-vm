9787230a3b9821eed2267f43f79b329eb2422773
Fabiano Florentino <florentino@ibm.com>
2019-07-27 16:54:43 -0300

    create tf files

---
+++ vars.tf
@@ -0,0 +1,43 @@
+#
+# Variables with default values, alter according to the your environment.
+#
+variable "data_center" {
+  default = "ha-datacenter"
+}
+
+variable "data_store" {
+  default = "vmdt-1"
+}
+
+variable "mgmt_lan" {
+  default = "VM Network"
+}
+
+variable "net_adapter_type" {
+  default = "vmxnet3"
+}
+
+variable "guest_id" {
+  default = "centos7_64Guest"
+}
+variable "custom_iso_path" {
+  default = "iso_images/custom-centos7-v7.iso"
+}
+variable "name_new_vm" {
+  description = "Input a name for Virtual Machine Ex. new_vm"
+  default     = ${params.name_vm}
+}
+variable "vm_count" {
+  description = "Number of instaces"
+  default     = ${params.number_of_machines}
+}
+
+variable "num_cpus" {
+  description = "Amount of vCPU's"
+  default     = ${params.cpu}
+}
+
+variable "num_mem" {
+  description = "Amount of Memory"
+  default     = ${params.memory}
+}
