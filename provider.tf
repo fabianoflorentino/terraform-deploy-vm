d5c0aaf2e34f169ca0f5c106c5f1a921f550b59e
Fabiano Santos Florentino <fabiano.sre@gmail.com>
2019-07-27 17:27:11 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 # Configure the vSphere Provider
 provider "vsphere" {
-  vsphere_server       = "esxi-node-1.br.horto.local"
+  vsphere_server       = "192.168.0.108"
   user                 = "fabiano"
   password             = "@1234qwer"
   allow_unverified_ssl = true
